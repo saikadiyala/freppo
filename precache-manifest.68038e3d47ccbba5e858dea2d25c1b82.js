7ddf62f59363abbefc0a5b08934eadcf254b4092
sai kadiyala <saikadiyala201@gmail.com>
2020-07-04 16:18:02 +0530

    Updates

---
+++ precache-manifest.68038e3d47ccbba5e858dea2d25c1b82.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4030665d04c658bb73f1f1d4a0cea582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "342f4cc435bbacaa4751",
+    "url": "/static/css/main.37032d62.chunk.css"
+  },
+  {
+    "revision": "a90914851b72d9a677ce",
+    "url": "/static/js/2.281d3307.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.281d3307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342f4cc435bbacaa4751",
+    "url": "/static/js/main.332a2b1a.chunk.js"
+  },
+  {
+    "revision": "899ca7c15e9ee1d71cbf",
+    "url": "/static/js/runtime-main.89bc9405.js"
+  },
+  {
+    "revision": "b000db64c9129e157549139960a6675a",
+    "url": "/static/media/1.b000db64.svg"
+  },
+  {
+    "revision": "6c6e75407d85f082ddccf8a5eca1eed0",
+    "url": "/static/media/2.6c6e7540.svg"
+  },
+  {
+    "revision": "d85a87369c40c3d767b9a961cd99097d",
+    "url": "/static/media/3.d85a8736.svg"
+  },
+  {
+    "revision": "0f67f3c99e672fbefe7ab3f21fb0441f",
+    "url": "/static/media/4.0f67f3c9.svg"
+  },
+  {
+    "revision": "b6e97a715882e9917b9d719db255c672",
+    "url": "/static/media/books-comic-homepage.b6e97a71.jpg"
+  },
+  {
+    "revision": "a3a6f80ade97ce4f5ef7c02f1bfb6d4d",
+    "url": "/static/media/books-general-ledger.a3a6f80a.png"
+  },
+  {
+    "revision": "6ac30b66c09b6fdb1bdd6986bca2ada9",
+    "url": "/static/media/dashboard.6ac30b66.png"
+  },
+  {
+    "revision": "133a7f394b76821c9613f713f52e8a85",
+    "url": "/static/media/frappe-team.133a7f39.webp"
+  },
+  {
+    "revision": "824b883de422d3754502349a11458eeb",
+    "url": "/static/media/frappebooks-hero.824b883d.png"
+  },
+  {
+    "revision": "55670e6b179ffbbac2858ffe7bbe6df6",
+    "url": "/static/media/invoicing.55670e6b.png"
+  },
+  {
+    "revision": "853ad7fcd3a0dd9fafbad459b09251d4",
+    "url": "/static/media/payments.853ad7fc.webp"
+  },
+  {
+    "revision": "61b55c65583a34daf2e1a82231c3b21d",
+    "url": "/static/media/report-pnl.61b55c65.png"
+  }
+]);
